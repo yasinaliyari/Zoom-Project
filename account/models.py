8f08a1ef15357e955340051131dad17bf90f359d
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-05 17:22:14 +0330

    change jitsi_url_path to zoom_url

--- account/models.py
+++ account/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=20)
-    jitsi_url_path = models.CharField(max_length=100)
+    zoom_url = models.CharField(max_length=100)
 
 
 class Account(User):
