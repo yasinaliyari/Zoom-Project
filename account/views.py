c6b8ec77c12427b56725d4f801106886ffe90cff
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-05 17:14:27 +0330

    strat project

---
+++ account/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    pass
+
+
+def signup(request):
+    pass
+
+
+def login_account(request):
+    pass
+
+
+def logout_account(request):
+    pass
+
+
+@login_required
+def joinoradd_team(request):
+    pass
+
+
+def exit_team(request):
+    pass
