34a685a3f38197be2555cae5e2584c232a31635f
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-05 19:36:44 +0330

    change names

--- account/forms.py
+++ account/forms.py
@@ -2,17 +2,16 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import Form, ModelForm
+from account.models import UserProfile, Team
 
-from .models import Account, Team
 
-
-class SignUpForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
     email = forms.EmailField(
         max_length=254, help_text="Required. Inform a valid email address."
     )
 
     class Meta:
-        model = Account
+        model = User
         fields = (
             "username",
             "email",
@@ -28,5 +27,5 @@ class TeamForm(ModelForm):
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=10)
+    username = forms.CharField(max_length=15)
     password = forms.CharField(max_length=32, widget=forms.PasswordInput)
